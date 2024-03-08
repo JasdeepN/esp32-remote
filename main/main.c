88b12c223eeedcef73b5411ba8484cf77fccc8f6
Jasdeep Nijjar <jasdeepn4@hotmail.com>
2024-03-08 13:57:49 -0500

    NTP time sync
    Added NTP time syncing so module pulls correct time from the internet.

--- main/main.c
+++ main/main.c
@@ -10,6 +10,7 @@
    //
    // For more information please see the general_user_settings.h file
 
+// weather station
 #include "sdkconfig.h"
 #include "general_user_settings.h"
 #include "secret_user_settings.h"
@@ -18,13 +19,13 @@
 #include <stdint.h>
 #include <math.h>
 
+#include <esp_sntp.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/semphr.h"
 #include "freertos/queue.h"
 #include "freertos/event_groups.h"
-
-#include <esp_system.h>
 
 #include "esp_wifi.h"
 #include "esp_log.h"
@@ -62,20 +63,6 @@
 #include "esp_wifi_he.h"
 #include "led_strip.h"
 
-static void blink_led(void);
-static void configure_led(void);
-static void set_led(int i, double s);
-static void startup(void);
-static void startup_validations_and_displays();
-static void initalize_non_volatile_storage();
-static void initialize_power_management();
-static void connect_to_WiFi();
-
-static void test(void);
-static void send_mqtt(void);
-
-static const char* TAG = "MyModule";
-
 struct colour {
    int red;
    int green;
@@ -89,6 +76,230 @@ struct colour {
    { 0,   0,   0   }, // off    5
    { 128, 128, 128 }, // white 50% power
 };
+
+// sntp example
+// #include <string.h>
+#include <time.h>
+#include <sys/time.h>
+#include "esp_system.h"
+// #include "esp_event.h"
+// #include "esp_log.h"
+#include "esp_attr.h"
+// #include "esp_sleep.h"
+// #include "nvs_flash.h"
+// #include "protocol_examples_common.h"
+#include "esp_netif_sntp.h"
+#include "lwip/ip_addr.h"
+#include "esp_sntp.h"
+
+void test(void);
+static void send_mqtt(void);
+static char* get_time(void);
+
+static void blink_led(void);
+static void configure_led(void);
+static void set_led(int i, double s);
+void startup(void);
+static void startup_validations_and_displays();
+static void initalize_non_volatile_storage();
+static void initialize_power_management();
+static void connect_to_WiFi();
+
+static void wait(int seconds);
+
+
+/* Variable holding number of times ESP32 restarted since first boot.
+ * It is placed into RTC memory using RTC_DATA_ATTR and
+ * maintains its value when ESP32 wakes from deep sleep.
+ */
+RTC_DATA_ATTR static int boot_count = 0;
+
+
+static void obtain_time(void);
+static const char *TAG = "time sntp";
+static const char *CONFIG_SNTP_TIME_SERVER = "time.nrc.ca";
+
+static const double INTENSITY = 0.10;
+
+#ifdef CONFIG_SNTP_TIME_SYNC_METHOD_CUSTOM
+void sntp_sync_time(struct timeval *tv)
+{
+   settimeofday(tv, NULL);
+   ESP_LOGI(TAG, "Time is synchronized from custom code");
+   sntp_set_sync_status(SNTP_SYNC_STATUS_COMPLETED);
+}
+#endif
+
+void time_sync_notification_cb(struct timeval *tv)
+{
+    ESP_LOGI(TAG, "Notification of a time synchronization event");
+}
+
+void init_ntp(void)
+{
+   set_led(3, INTENSITY);
+    time_t now;
+    struct tm timeinfo;
+    time(&now);
+    localtime_r(&now, &timeinfo);
+    // Is time set? If not, tm_year will be (1970 - 1900).
+    if (timeinfo.tm_year < (2016 - 1900)) {
+        ESP_LOGI(TAG, "Time is not set yet. Connecting to WiFi and getting time over NTP.");
+        obtain_time();
+        // update 'now' variable with current time
+        time(&now);
+    }
+#ifdef CONFIG_SNTP_TIME_SYNC_METHOD_SMOOTH
+    else {
+        // add 500 ms error to the current system time.
+        // Only to demonstrate a work of adjusting method!
+      //   {
+      //       ESP_LOGI(TAG, "Add a error for test adjtime");
+      //       struct timeval tv_now;
+      //       gettimeofday(&tv_now, NULL);
+      //       int64_t cpu_time = (int64_t)tv_now.tv_sec * 1000000L + (int64_t)tv_now.tv_usec;
+      //       int64_t error_time = cpu_time + 500 * 1000L;
+      //       struct timeval tv_error = { .tv_sec = error_time / 1000000L, .tv_usec = error_time % 1000000L };
+      //       settimeofday(&tv_error, NULL);
+      //   }
+
+        ESP_LOGI(TAG, "Time was set, now just adjusting it. Use SMOOTH SYNC method.");
+        obtain_time();
+        // update 'now' variable with current time
+        time(&now);
+    }
+#endif
+
+    char strftime_buf[64];
+
+    // Set timezone to Eastern Standard Time and print local time
+    setenv("TZ", "EST5EDT,M3.2.0/2,M11.1.0", 1);
+    tzset();
+    localtime_r(&now, &timeinfo);
+    strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+    ESP_LOGI(TAG, "The current date/time in Toronto is: %s", strftime_buf);
+
+
+    if (sntp_get_sync_mode() == SNTP_SYNC_MODE_SMOOTH) {
+        struct timeval outdelta;
+        while (sntp_get_sync_status() == SNTP_SYNC_STATUS_IN_PROGRESS) {
+            adjtime(NULL, &outdelta);
+            ESP_LOGI(TAG, "Waiting for adjusting time ... outdelta = %jd sec: %li ms: %li us",
+                        (intmax_t)outdelta.tv_sec,
+                        outdelta.tv_usec/1000,
+                        outdelta.tv_usec%1000);
+            vTaskDelay(2000 / portTICK_PERIOD_MS);
+        }
+    }
+   set_led(1, INTENSITY);
+
+}
+
+static void print_servers(void)
+{
+    ESP_LOGI(TAG, "List of configured NTP servers:");
+
+    for (uint8_t i = 0; i < SNTP_MAX_SERVERS; ++i){
+        if (esp_sntp_getservername(i)){
+            ESP_LOGI(TAG, "server %d: %s", i, esp_sntp_getservername(i));
+        } else {
+            // we have either IPv4 or IPv6 address, let's print it
+            char buff[INET6_ADDRSTRLEN];
+            ip_addr_t const *ip = esp_sntp_getserver(i);
+            if (ipaddr_ntoa_r(ip, buff, INET6_ADDRSTRLEN) != NULL)
+                ESP_LOGI(TAG, "server %d: %s", i, buff);
+        }
+    }
+}
+
+static void obtain_time(void)
+{
+    ESP_ERROR_CHECK( nvs_flash_init() );
+    ESP_ERROR_CHECK(esp_netif_init());
+   //  ESP_ERROR_CHECK( esp_event_loop_create_default() );
+
+#if LWIP_DHCP_GET_NTP_SRV
+    /**
+     * NTP server address could be acquired via DHCP,
+     * see following menuconfig options:
+     * 'LWIP_DHCP_GET_NTP_SRV' - enable STNP over DHCP
+     * 'LWIP_SNTP_DEBUG' - enable debugging messages
+     *
+     * NOTE: This call should be made BEFORE esp acquires IP address from DHCP,
+     * otherwise NTP option would be rejected by default.
+     */
+    ESP_LOGI(TAG, "Initializing SNTP");
+    esp_sntp_config_t config = ESP_NETIF_SNTP_DEFAULT_CONFIG(CONFIG_SNTP_TIME_SERVER);
+    config.start = false;                       // start SNTP service explicitly (after connecting)
+    config.server_from_dhcp = true;             // accept NTP offers from DHCP server, if any (need to enable *before* connecting)
+    config.renew_servers_after_new_IP = true;   // let esp-netif update configured SNTP server(s) after receiving DHCP lease
+    config.index_of_first_server = 1;           // updates from server num 1, leaving server 0 (from DHCP) intact
+    // configure the event on which we renew servers
+    config.ip_event_to_renew = IP_EVENT_STA_GOT_IP;
+   //  config.ip_event_to_renew = IP_EVENT_ETH_GOT_IP;
+    config.sync_cb = time_sync_notification_cb; // only if we need the notification function
+    esp_netif_sntp_init(&config);
+
+#endif /* LWIP_DHCP_GET_NTP_SRV */
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK( esp_wifi_connect() );
+
+#if LWIP_DHCP_GET_NTP_SRV
+    ESP_LOGI(TAG, "Starting SNTP");
+    esp_netif_sntp_start();
+#if LWIP_IPV6 && SNTP_MAX_SERVERS > 2
+    /* This demonstrates using IPv6 address as an additional SNTP server
+     * (statically assigned IPv6 address is also possible)
+     */
+    ip_addr_t ip6;
+    if (ipaddr_aton("2a01:3f7::1", &ip6)) {    // ipv6 ntp source "ntp.netnod.se"
+        esp_sntp_setserver(2, &ip6);
+    }
+#endif  /* LWIP_IPV6 */
+
+#else
+    ESP_LOGI(TAG, "Initializing and starting SNTP");
+#if CONFIG_LWIP_SNTP_MAX_SERVERS > 1
+    /* This demonstrates configuring more than one server
+     */
+    esp_sntp_config_t config = ESP_NETIF_SNTP_DEFAULT_CONFIG_MULTIPLE(2,
+                               ESP_SNTP_SERVER_LIST(CONFIG_SNTP_TIME_SERVER, "pool.ntp.org" ) );
+#else
+    /*
+     * This is the basic default config with one server and starting the service
+     */
+    esp_sntp_config_t config = ESP_NETIF_SNTP_DEFAULT_CONFIG(CONFIG_SNTP_TIME_SERVER);
+#endif
+    config.sync_cb = time_sync_notification_cb;     // Note: This is only needed if we want
+#ifdef CONFIG_SNTP_TIME_SYNC_METHOD_SMOOTH
+    config.smooth_sync = true;
+#endif
+
+    esp_netif_sntp_init(&config);
+#endif
+
+    print_servers();
+
+    // wait for time to be set
+    time_t now = 0;
+    struct tm timeinfo = { 0 };
+    int retry = 0;
+    const int retry_count = 15;
+    while (esp_netif_sntp_sync_wait(2000 / portTICK_PERIOD_MS) == ESP_ERR_TIMEOUT && ++retry < retry_count) {
+        ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
+    }
+    time(&now);
+    localtime_r(&now, &timeinfo);
+
+   //  ESP_ERROR_CHECK( esp_wifi_disconnect() );
+    esp_netif_sntp_deinit();
+}
+
+/// end
 
 #define BLINK_GPIO CONFIG_BLINK_GPIO
 
@@ -153,6 +364,7 @@ void startup(void){
    // initialize_the_external_switch();
 
    connect_to_WiFi();
+   init_ntp();
    ESP_LOGI("Startup", "Complete");
    set_led(1, INTENSITY);
 
@@ -1237,16 +1449,39 @@ void test()
 }
 static void send_mqtt()
 {
-    static char topic[100];
+    static char topic[50];
     strcpy(topic, "test");
     strcat(topic, "/");
     strcat(topic, "1");
 
-    static char payload[20];
-    sprintf(payload, "%s", "hi from ESP32 C6!");
+    static char payload[200]; // can shrink this 
+    sprintf(payload, "%s at time %s", "hi from ESP32 C6!", get_time());
 
     ESP_LOGI("MQTT", "publish: %s %s", topic, payload);
     esp_mqtt_client_publish(MQTT_client, topic, payload, 0, GENERAL_USER_SETTINGS_MQTT_QOS, GENERAL_USER_SETTINGS_MQTT_RETAIN);
+    
+}
+
+static char* get_time(){
+   time_t mytime = time(NULL);
+   char * time_str = ctime(&mytime);
+   time_str[strlen(time_str)-1] = '\0';
+   // printf("Current Time : %s\n", time_str);
+   static char payload[100]; // can shrink this 
+   sprintf(payload, "Current Time : %s", time_str);
+
+   return payload;
+}
+
+static void wait(int seconds){
+   ESP_LOGI("WAIT", "Sleeping for %d seconds..", seconds);
+
+   volatile int ms = seconds*1000;
+   set_led(0, 0.10);
+   vTaskDelay(ms / portTICK_PERIOD_MS);
+   ESP_LOGI("WAIT", "%s", "Wake up!");
+   set_led(1, 0.10);
+
 }
 
 void app_main(void)
@@ -1261,10 +1496,10 @@ void app_main(void)
    //  }
 
 
-      test();
 
-   //  while (true)
-   //  {
+    while (true)
+    {
+      test();
    //      get_bme680_readings();
 
    //      if (BME680_readings_are_reasonable)
@@ -1276,7 +1511,9 @@ void app_main(void)
    //          ESP_LOGE("BME680", "couldn't get a valid reading from the BME680; please check the wiring;");
    //          restart_after_this_many_seconds(120);
    //      };
+        wait(60);
 
-        goto_sleep();
-   //  }
+
+      //   goto_sleep();
+    }
 }
